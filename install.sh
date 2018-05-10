9d4ad5bba04e5eb806c464d43218d6cce81ecb59
Felipe Leão <felipe@felipeleao.com>
2018-05-09 21:35:07 -0300

    chore: Remove programs and only keep dotfiles

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Basic Install function
-basicInstall() {
-	echo -e "\e[32mInstalling $1\e[m"
-	sudo apt -y -qq install $1
-}
-
-# Install System Packages
-source ./applications/system.sh
-
 # Sync dotfiles
 syncAll() {
     rsync --exclude ".git/" --exclude "install.sh" --exclude "readme.md" -avh --no-perms . ~;
